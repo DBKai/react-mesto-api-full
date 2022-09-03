3f42c956e993be5de1cd60753db37a0db9f36567
Dmitriy Kaygorodov <dbkai@ya.ru>
2022-09-03 11:17:19 +0500

    feat: added express-rate-limit

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const cors = require('cors');
 const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const { indexRouters } = require('./src/routes');
 const { errorsHandler } = require('./src/middlewares/errorsHandler');
 const { requestLogger, errorLogger } = require('./src/middlewares/logger');
@@ -13,6 +14,14 @@ const corsOptionsDelegate = require('./src/middlewares/cors');
 const { PORT = 3000 } = process.env;
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+app.use(limiter);
 app.use(express.json());
 app.use(cookieParser());
 app.use(requestLogger);
